05f080f690f4d13f46b0213ed1a5bc43b5356e29
Danilo Rojas Pamplona <danilo.rojas@premexcorp.com>
2023-04-10 19:40:07 -0500

    Initial commit

---
+++ app/src/main/java/com/movilespascual/recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.movilespascual.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ListaElementos> elementos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void  init(){
+        elementos = new ArrayList<>();
+        //elementos.add(new ListaElementos("#7B1FA2","Pedro","Ciudad de México","Activo"));
+        
+
+        ListAdapter listAdapter = new ListAdapter(elementos,this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+
+}
